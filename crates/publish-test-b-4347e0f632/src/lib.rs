f25d621d1d16e9b8053cf7740a0f058e3dd13086
Benoît CORTIER <bcortier@pm.me>
2022-04-25 19:04:42 -0400

    feat: super function 3

--- crates/publish-test-b-4347e0f632/src/lib.rs
+++ crates/publish-test-b-4347e0f632/src/lib.rs
@@ -10,6 +10,10 @@ pub fn super_function_2() -> &'static str {
     "SUPER VALUE"
 }
 
+pub fn super_function_3() -> &'static str {
+    "SUPER VALUE"
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
