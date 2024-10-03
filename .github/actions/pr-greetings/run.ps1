37c82ac2f9b70070e8f5f6cda7f95eb426aa6f60
Benoît CORTIER <bcortier@proton.me>
2024-10-03 15:56:47 +0900

    fixup

--- .github/actions/pr-greetings/run.ps1
+++ .github/actions/pr-greetings/run.ps1
@@ -19,6 +19,31 @@ $MyInvocation.MyCommand.Parameters `
         @{ Label = "Value"; Expression = { try { (Get-Variable -Name $_.Key).Value } catch { "" } }; }
 Write-Host
 
+function Invoke-Cmd
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Name,
+        [Parameter(Mandatory=$true)]
+        [string[]] $Args,
+        [switch] $IgnoreFailure
+    )    
+
+    Write-Host ">> Invoke '${Name}' $($Args | Join-String -FormatString '''{0}''' -Separator ' ')"
+
+    # Workaround: temporary change error action preferance because until v7.2 stderr redirection (>2) is redirected to the PowerShell error stream
+    $ErrorActionPreference = "Continue"
+    & $Name $Args 2>&1
+    $ErrorActionPreference = "Stop"
+
+    $failed = $LastExitCode -ne 0
+
+    if ($failed -and (-not $IgnoreFailure))
+    {
+        throw "${Name} invocation failed"
+    }
+}
+
 Write-Host ">> Read template file at $TemplatePath"
 
 $body = Get-Content -Path "$TemplatePath"
